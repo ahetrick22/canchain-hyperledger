07741d6e36328ae9668ab9d13673cb5fa8ad75a7
Alicia Hetrick <ahetrick22@gmail.com>
2019-03-17 13:28:25 -0400

    contract successfully instantiates

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const BagCount = require('./lib/BagCount');
+
+module.exports.BagCount = BagCount;
+module.exports.contracts = [ BagCount ];
